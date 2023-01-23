42fd5686337e3044c03028c50617e3a9761866d0
julia.cavalcante <ccardozojulia@gmail.com>
2023-01-23 14:38:05 -0300

    FindNWidgets test

--- test/home_test.dart
+++ test/home_test.dart
@@ -1,3 +1,4 @@
+import 'package:estilizacao_componentes/components/box_card.dart';
 import 'package:estilizacao_componentes/data/bank_inherited.dart';
 import 'package:estilizacao_componentes/screens/home.dart';
 import 'package:flutter/material.dart';
@@ -30,5 +31,20 @@ void main() {
       ),
     ));
     expect(find.byKey(Key('testKey')), findsOneWidget);
+  });
+  
+  testWidgets('Find 5 BoxCards', (tester) async{
+    await tester.pumpWidget(MaterialApp(
+      home: BankInherited(
+        child: Home(),
+      ),
+    ));
+    expect(find.byWidgetPredicate((widget) {
+      if(widget is BoxCard){
+        return true;
+      }else{
+        return false;
+      }
+    }), findsNWidgets(5));
   });
 }

d27d68463a1b04f9b714b446ece7ab8ea3af4ec1
julia.cavalcante <ccardozojulia@gmail.com>
2023-01-23 11:52:53 -0300

    first unity test

---
+++ test/bank_model_test.dart
@@ -0,0 +1,11 @@
+import 'package:estilizacao_componentes/models/bank.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+
+void main(){
+  test('Bank model deposit should be 10', () {
+    final bank = BankModel();
+    bank.deposit(10);
+    expect(bank.points, 10);
+  });
+}
